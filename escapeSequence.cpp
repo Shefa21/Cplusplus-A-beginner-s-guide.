75f61d89bbc41542141457232b2e95d5b3193bc7
Shefa21 <152915092+Shefa21@users.noreply.github.com>
2024-02-21 12:12:15 +0600

    Create escapeSequence.cpp

---
+++ escapeSequence.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"one\ttwo\tthree\n";
+    cout<<"123\b\b45"; // 2 backspace characters deleted 2 and 3 same as 12\b3\b45
+    return 0;
+}

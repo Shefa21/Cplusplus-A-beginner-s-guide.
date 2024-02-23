791e9206021bb37416cea8fa5d5554389e911d41
Shefa21 <152915092+Shefa21@users.noreply.github.com>
2024-02-23 15:10:23 +0600

    Create convertToUpppercase.cpp

---
+++ convertToUpppercase.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char str[80];
+    int i;
+    strcpy(str, "this is a test");
+
+    for(i=0; str[i]; i++)
+        str[i]=toupper(str[i]);
+    cout<<str;
+    return 0;
+}
+
+
+

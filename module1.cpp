9cab2e2158c4c0aed22cec2c513d3ad8aae54931
Shefa21 <152915092+Shefa21@users.noreply.github.com>
2024-02-25 01:18:15 +0600

    Update module1.cpp

--- module1.cpp
+++ module1.cpp
@@ -1,1 +1,56 @@
-module
+module 1
+  1. It has been said that C++ sits at the center of the modern programming universe. Explain this
+statement.
+2. A C++ compiler produces object code that is directly executed by the computer. True or false?
+3. What are the three main principles of object-oriented programming?
+4. Where do C++ programs begin execution?
+5. What is a header?
+6. What is <iostream>? What does the following code do?
+#include <iostream>
+7. What is a namespace?
+8. What is a variable?
+9. Which of the following variable names is/are invalid?
+41 C++ A Beginner’s Guide by Herbert Schildt
+a. count
+b. _count
+c. count27
+d. 67count
+e. if
+10. How do you create a single-line comment? How do you create a multiline comment?
+11. Show the general form of the if statement. Show the general form of the for loop.
+12. How do you create a block of code?
+13. The moon’s gravity is about 17 percent that of Earth’s. Write a program that displays a table that
+shows Earth pounds and their equivalent moon weight. Have the table run from 1 to 100 pounds.
+Output a newline every 25 pounds.
+14. A year on Jupiter (the time it takes for Jupiter to make one full circuit around the Sun) takes about
+12 Earth years. Write a program that converts Jovian years to Earth years. Have the user specify the
+number of Jovian years. Allow fractional years.
+15. When a function is called, what happens to program control?
+16. Write a program that averages the absolute value of five values entered by the user. Display the
+result
+1. C++ is at the center of modern programming because it was derived from C and is the parent of Java
+and C#. These are the four most important programming languages.
+2. True, a C++ compiler produces code that can be directly executed by the computer.
+3. Encapsulation, polymorphism, and inheritance are the three guiding principles of OOP.
+4. C++ programs begin execution at main( ).
+5. A header contains information used by the program.
+6. <iostream> is the header the supports I/O. The statement includes the <iostream> header in a
+program.
+7. A namespace is a declarative region in which various program elements can be placed. Elements
+declared in one namespace are separate from elements declared in another.
+8. A variable is a named memory location. The contents of a variable can be changed during the
+execution of a program.
+9. The invalid variables are d and e. Variable names cannot begin with a digit or be the same as a C++
+keyword.
+10. A single-line comment begins with // and ends at the end of the line. A multiline comment begins
+with /* and ends with */.
+11. The general form of the if:
+if(condition) statement;
+The general form of the for:
+for(initialization; condition; increment) statement;
+12. A block of code is started with a { and ended with a }.
+13. // Show a table of Earth to Moon weights.
+2 C++ A Beginner’s Guide by Herbert Schildt
+14. // Convert Jovian years to Earth years.
+15. When a function is called, program control transfers to that function.
+16. // Average the absolute values of 5 number

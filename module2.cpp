9222b14c054ceaec5a3c0d4ba4e9b221e64327b6
Shefa21 <152915092+Shefa21@users.noreply.github.com>
2024-02-25 01:20:23 +0600

    Update module2.cpp

--- module2.cpp
+++ module2.cpp
@@ -1,1 +1,41 @@
-module 2
+1. What type of integers are supported by C++?
+2. By default, what type is 12.2?
+32 C++ A Beginner’s Guide by Herbert Schildt
+3. What values can a bool variable have?
+4. What is the long integer data type?
+5. What escape sequence produces a tab? What escape sequence rings the bell?
+6. A string is surrounded by double quotes. True or false?
+7. What are the hexadecimal digits?
+8. Show the general form for initializing a variable when it is declared.
+9. What does the % do? Can it be used on floating-point values?
+10. Explain the difference between the prefix and postfix forms of the increment operator.
+11. Which of the following are logical operators in C++?
+a. &&
+b. ##
+c. ||
+d. $$
+e. !
+12. How can
+x = x + 12;
+be rewritten?
+13. What is a cast?
+14. Write a program that finds all of the prime numbers between 1 and 100
+  1. The C++ integer types are
+The type char can also be used as an integer type.
+2. 12.2 is type double.
+3. A bool variable can be either true or false.
+4. The long integer type is long int, or just long.
+5. The \t sequence represents a tab. The \b rings the bell.
+6. True, a string is surrounded by double quotes.
+7. The hexadecimal digits are 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F.
+4 C++ A Beginner’s Guide by Herbert Schildt
+8. To initialize a variable, use this general form:
+type var = value;
+9. The % is the modulus operator. It returns the remainder of an integer division. It cannot be used
+on floating-point values.
+10. When the increment operator precedes its operand, C++ will perform the corresponding
+operation prior to obtaining the operand's value for use by the rest of the expression. If the
+operator follows its operand, then C++ will obtain the operand's value before incrementing.
+11. A, C, and E
+12. x += 12;
+13. A cast is an explicit type conversio
